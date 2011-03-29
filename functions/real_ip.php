1b206792a786f10e127197b33c9c2467cc64da71
David Lewis <highwayoflife@gmail.com>
2011-03-29 11:55:10 -0700

    Adding real_ip function

---
+++ functions/real_ip.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Returns the real IP address of the user
+ */
+function real_ip()
+{
+    // No IP found (will be overwritten by for
+    // if any IP is found behind a firewall)
+    $ip = FALSE;
+
+    // If HTTP_CLIENT_IP is set, then give it priority
+    if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+	{
+        $ip = $_SERVER["HTTP_CLIENT_IP"];
+    }
+
+    // User is behind a proxy and check that we discard RFC1918 IP addresses if they
+    // are behind a proxy then only figure out which IP belongs to the user.
+    // Might not need any more hackin if there is a squid reverse proxy in front of apache.
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+	{
+        // Put the IP's into an array which we shall work with shortly.
+        $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+        if ($ip)
+		{
+			array_unshift($ips, $ip);
+			$ip = FALSE;
+		}
+
+        for ($i = 0; $i < count($ips); $i++)
+		{
+            // Skip RFC 1918 IP's 10.0.0.0/8, 172.16.0.0/12 and 192.168.0.0/16
+            if (!preg_match('/^(?:10|172\.(?:1[6-9]|2\d|3[01])|192\.168)\./', $ips[$i]))
+			{
+                if (version_compare(phpversion(), "5.0.0", ">="))
+				{
+                    if (ip2long($ips[$i]) != false)
+					{
+                        $ip = $ips[$i];
+                        break;
+                    }
+                }
+				else
+				{
+                    if (ip2long($ips[$i]) != -1)
+					{
+                        $ip = $ips[$i];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Return with the found IP or the remote address
+    return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+}
